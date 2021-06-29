3558af451e4a774d672753c522a12bd88ad71d9b
me <20161564@vision.hoseo.edu>
2021-06-29 20:26:50 +0900

    에러 알림 컴포넌트 구현

--- src/components/Dialog/Warning.js
+++ src/components/Dialog/Warning.js
@@ -2,25 +2,33 @@ import React from 'react'
 import DialogContent from '@material-ui/core/DialogContent'
 import Dialog from '@material-ui/core/Dialog'
 // import Avatar from '@material-ui/core/Avatar'
-import WarningIcon from '@material-ui/icons/Warning';
+import WarningIcon from 'assets/img/icons/common/warning.png';
 
 export default function Warning(props) {
     const { onClose, open, children } = props
     const handleClose = () => onClose() 
     const textStyle = {
-        display:'inline-block',
-        marginLeft:'30px',
-        color:'black'
+        color:'black',
+        paddingLeft:'30px'
     }
     return (
         <Dialog 
-            PaperProps= {{style : {backgroundColor:"pink", }}}
+            PaperProps= {{style : {backgroundColor:"#F6F4ED", }}}
             maxWidth="xs"
             onClose={handleClose}
             open={open}>
             <DialogContent>
-                <WarningIcon color="primary"/>
-                <span style={textStyle}>{children}</span>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td><img src={WarningIcon} style={{
+                                width : '20px',
+                                height : '20px'
+                            }}/></td>
+                            <td style={textStyle}>{children}</td>
+                        </tr>
+                    </tbody>                    
+                </table>
             </DialogContent>
         </Dialog>
     )
