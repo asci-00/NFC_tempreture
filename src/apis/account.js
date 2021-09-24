3c263d79567572f8a6ac8f0bb63baec37fb6fae2
w_lan123@naver.com <asci-00>
2021-09-25 01:21:31 +0900

    Update: account api 수정 및 추가

--- src/apis/account.js
+++ src/apis/account.js
@@ -1,5 +1,9 @@
 import Api from './Api'
 
-export const getRequest = () => Api.get('/v1/useradmin/account/lookup')
-export const setRequest = ({type, data}) => Api.post('/v1/useradmin/subgroup/authadd', { type, data})
-export const deleteRequest = (uuid) => Api.delete('/v1/useradmin/account/del', {uuid})
+export const getRequest = () => Api.get('/v1/useradmin/account')
+export const approveRequest = (uuid, groupCode) => Api.post('/v1/useradmin/subgroup/authadd', { targetUUID : uuid, targetGroupCode : groupCode })
+export const revokeRequest = (uuid) => Api.post('/v1/useradmin/subgroup/authremove', { targetUUID : uuid })
+export const requestNewAccountGroup = (displayname, Group_name, address) => Api.post('/v1/useradmin/subgroup/group', { displayname, Group_name, address })
+export const requestNewAccount = (displayname, Group_name) => Api.post('/v1/useradmin/subgroup/user', { displayname, Group_name })
+export const updateRequest = (uuid) => Api.put('/v1/useradmin/accountMGMT', {targetUUID : uuid})
+export const deleteRequest = (uuid) => Api.delete('/v1/useradmin/accountMGMT', {targetUUID : uuid})
