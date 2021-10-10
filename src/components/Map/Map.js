2e3525c5d9487edcdfde042d85efe6a2300b97cb
NE5054 <w_lan123@naver.com>
2021-10-10 23:15:25 +0900

    Dev: Preview Data Update

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,59 +1,73 @@
 import React from "react";
 // @material-ui/core components
 import Box from "@material-ui/core/Box";
-import { styles } from 'modules/static/map'
+import { styles } from "modules/static/map";
 
-export default function MapWrapper ({data}) {
-    const mapRef = React.useRef(null);    
-    
-    React.useEffect(() => {
-      let google = window.google;
-      let map = mapRef.current;
-  
-      const myLatlng = new google.maps.LatLng(36.736134, 127.077176);
-      console.log(myLatlng)
-      const mapOptions = {
-        zoom: 15,
-        center: myLatlng,
-        scrollwheel: true,
-        zoomControl: true,
-        styles
-      };
-  
-      map = new google.maps.Map(map, mapOptions);
-      // const flightPath = new google.maps.Polyline({
-      //   path: data,
-      //   geodesic: true,
-      //   strokeColor: "#FF0000",
-      //   strokeOpacity: 1.0,
-      //   strokeWeight: 2,
-      // }).setMap(map)
-      
-      if(data) data.forEach((log, idx) => {
-        const {latitude : lat, longitude : lng, building_name, dateTime} = log
+export default function MapWrapper({ data }) {
+  const mapRef = React.useRef(null);
+
+  React.useEffect(() => {
+    let google = window.google;
+    let map = mapRef.current;
+
+    const myLatlng = new google.maps.LatLng(36.736134, 127.077176);
+    console.log(myLatlng);
+    const mapOptions = {
+      zoom: 15,
+      center: myLatlng,
+      scrollwheel: true,
+      zoomControl: true,
+      styles,
+    };
+
+    map = new google.maps.Map(map, mapOptions);
+    // const flightPath = new google.maps.Polyline({
+    //   path: data,
+    //   geodesic: true,
+    //   strokeColor: "#FF0000",
+    //   strokeOpacity: 1.0,
+    //   strokeWeight: 2,
+    // }).setMap(map)
+
+    if (data)
+      data.forEach((log, idx) => {
+        const { latitude: lat, longitude: lng, building_name, dateTime } = log;
         const marker = new google.maps.Marker({
-            position: {lat, lng},
-            map: map,
-            animation: google.maps.Animation.DROP,
-            title: building_name,
-        })
-        const contentString =
-        `<div class="info-window-content"><h2>${building_name}</h2>
+          position: { lat, lng },
+          map: map,
+          animation: google.maps.Animation.DROP,
+          title: building_name,
+        });
+        const contentString = `<div class="info-window-content"><h2>${building_name}</h2>
         <p>${dateTime}</p></div>`;
-        const infowindow = new google.maps.InfoWindow({ content: contentString, });
-        google.maps.event.addListener(marker, "click", function () { infowindow.open(map, marker) })
-      })
+        const infowindow = new google.maps.InfoWindow({
+          content: contentString,
+        });
+        google.maps.event.addListener(marker, "click", function () {
+          infowindow.open(map, marker);
+        });
+      });
+    //preview code
+    const marker = new google.maps.Marker({
+      position: { lat : 36.736134, lng : 127.077176 },
+      map: map,
+      animation: google.maps.Animation.DROP,
+      title: '호서대학교',
     });
-    return (
-      <>
-        <Box
-          height="600px"
-          position="relative"
-          width="100%"
-          overflow="hidden"
-          borderRadius=".375rem"
-          ref={mapRef}
-        ></Box>
-      </>
-    );
-  };
+    const contentString = `<div class="info-window-content"><h2>${'호서대학교'}</h2></div>`;
+    const infowindow = new google.maps.InfoWindow({ content: contentString });
+    infowindow.open(map, marker);
+  });
+  return (
+    <>
+      <Box
+        height="600px"
+        position="relative"
+        width="100%"
+        overflow="hidden"
+        borderRadius=".375rem"
+        ref={mapRef}
+      ></Box>
+    </>
+  );
+}
