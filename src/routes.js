1fc7967b9201de7fb65a4e78234f39fd2dc01567
NE5054 <w_lan123@naver.com>
2021-10-24 22:06:26 +0900

    Update: Router 정보 업데이트

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,4 @@
 // core components
-import Dashboard from "views/admin/Dashboard.js";
-import Login from "views/auth/Login.js";
-import UserManage from "views/admin/UserManage.js";
-import Profile from "views/admin/Profile.js";
-import Register from "views/auth/Register.js";
-import Account from "views/admin/Account.js";
-import Kiosk from "views/admin/Kiosk.js";
 // @material-ui/icons components
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import FormatListBulleted from "@material-ui/icons/FormatListBulleted";
@@ -13,6 +6,13 @@ import LocationOn from "@material-ui/icons/LocationOn";
 import Person from "@material-ui/icons/Person";
 import Tv from "@material-ui/icons/Tv";
 import VpnKey from "@material-ui/icons/VpnKey";
+import Account from "views/admin/Account.js";
+import Dashboard from "views/admin/Dashboard.js";
+import Kiosk from "views/admin/Kiosk.js";
+import Profile from "views/admin/Profile.js";
+import UserManage from "views/admin/UserManage.js";
+import Login from "views/auth/Login.js";
+import Register from "views/auth/Register.js";
 
 var routes = [
   {
@@ -50,7 +50,9 @@ var routes = [
     iconColor: "WarningLight",
     component: Profile,
     layout: "/admin",
-  }, {
+  },
+
+  {
     path: "/index",
     name: "대시보드",
     icon: Tv,
@@ -58,6 +60,22 @@ var routes = [
     component: Dashboard,
     layout: "/user",
   }, {
+    path: "/tables",
+    name: "Kiosk관리",
+    icon: FormatListBulleted,
+    iconColor: "Error",
+    component: Kiosk,
+    layout: "/user",
+  }, {
+    path: "/user-profile",
+    name: "정보변경",
+    icon: Person,
+    iconColor: "WarningLight",
+    component: Profile,
+    layout: "/user",
+  },
+
+  {
     path: "/login",
     name: "Login",
     icon: VpnKey,
